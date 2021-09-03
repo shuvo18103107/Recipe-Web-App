18d5ab3fc764244a90317eb7888afaa5b98bdbc3
shuvo18103107 <18103107@iubat.edu>
2021-09-03 21:04:20 +0600

    ✅ parcel bundling and project configuration

---
+++ src/js/controller.js
@@ -0,0 +1,9 @@
+const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+    return new Promise(function (_, reject) {
+        setTimeout(function () {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
+};
