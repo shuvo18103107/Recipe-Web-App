02a1dfab91c4b441dbd4590c1492e2886e81bece
shuvo18103107 <18103107@iubat.edu>
2021-09-04 16:06:25 +0600

    👽️ Loading a Recipe From API

--- dist/index.0e47d5c3.js
+++ dist/index.0e47d5c3.js
@@ -463,5 +463,31 @@ const timeout = function(s) {
         }, s * 1000);
     });
 };
+const showRecipe = async function() {
+    try {
+        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc990`);
+        const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        // console.log(res, data);
+        //recipe object property change and give a nice name to understand
+        let { recipe  } = data.data;
+        //now create new recipe object based on that
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        };
+        console.log(recipe);
+    } catch (err) {
+        alert(err.message);
+        console.log(err.message);
+    }
+};
+showRecipe();
 
 //# sourceMappingURL=index.0e47d5c3.js.map
