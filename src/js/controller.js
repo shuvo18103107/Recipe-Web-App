0fce24f85a327fe8b1fb1c42c41a19704673fe11
shuvo18103107 <18103107@iubat.edu>
2021-09-05 08:49:34 +0600

    📝 Listening for load and hashcange event

--- src/js/controller.js
+++ src/js/controller.js
@@ -27,10 +27,13 @@ const renderSpinner = function (parentEl) {
 };
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+    if (!id) return;
     //1.loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc13`
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
 
     const data = await res.json();
@@ -151,4 +154,6 @@ const showRecipe = async function () {
     console.log(err.message);
   }
 };
-showRecipe();
+
+//if i have multiple event for same dunctionality then i can doi this
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
